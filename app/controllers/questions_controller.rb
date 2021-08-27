833de45380f50145784f8681fc019175c9b22a99
Kevin Hookumchand <khookumchand@gmail.com>
2021-08-26 20:32:28 -0400

    Missing option part

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,16 @@
 class QuestionsController < ApplicationController
   def ask
-    @ask
+    
   end
 
   def answer
-  @answer
+    @question = params[:question]
+    if @question.end_with? '?' 
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @question == 'i am going to work right now!'
+      @answer = 'Great!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work'
+    end
   end
 end
